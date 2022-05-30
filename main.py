6bfb932ffd5890c0f7d4e51c85c177be27444608
gom1n <alice436777@gmail.com>
2022-05-31 04:02:19 +0900

    [데이터 정제] 지역별 충전소 지도에 점으로 출력

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ decoding = 'wEOWhOEV95XFLOJ4fqYdWXwJMODD6Ze7R8/QDUZdmBrHnlVU8ER0P2YenWEkb4imfh7I
 
 # url 입력
 url = 'http://apis.data.go.kr/B552584/EvCharger/getChargerInfo'
-params = {'serviceKey': decoding, 'pageNo' : '1', 'numOfRows' : '9999'}
+params = {'serviceKey': decoding, 'pageNo': '1', 'numOfRows': '9999'}
 
 response = requests.get(url, params=params)
 # xml 내용
@@ -56,6 +56,7 @@ for i in range(0, len(rows)):
 # pd.set_option('display.max_columns', None)
 station_df = pd.DataFrame(row_list, columns=name_list)
 
+
 loc_df = station_df.groupby('zcode')
 only_zcode = loc_df.size()
 print(only_zcode)
@@ -66,19 +67,27 @@ import platform
 # Window
 if platform.system() == 'Windows':
     plt.rc('font', family='Malgun Gothic')
-elif platform.system() == 'Darwin': # Mac
+elif platform.system() == 'Darwin':  # Mac
     plt.rc('font', family='AppleGothic')
-else: #linux
+else:  # linux
     plt.rc('font', family='NanumGothic')
 
-zcode_arr = ['11', '26', '27', '28', '29', '30', '31', '36', '41', '42', '43', '44', '45', '46', '47', '48', '50']
+plt.rc('font', size=20)        # 기본 폰트 크기
+plt.rc('axes', labelsize=20)   # x,y축 label 폰트 크기
+plt.rc('xtick', labelsize=6)  # x축 눈금 폰트 크기
+plt.rc('ytick', labelsize=7)  # y축 눈금 폰트 크기
+plt.rc('legend', fontsize=20)  # 범례 폰트 크기
+plt.rc('figure', titlesize=50) # figure title 폰트 크기
+
+zcode_arr = ['서울', '부산', '대구', '인천', '광주', '대전', '울산', '세종', '경기', '강원', '충북', '충남', '전북', '전남', '경북', '경남', '제주']
 df_count = only_zcode.to_numpy()
 
-# 숫자 labe 추가
-def add_value_label(x_list,y_list):
-    for i in range(1, len(x_list)+1):
-        plt.text(i,y_list[i-1],y_list[i-1], fontsize=5, color='blue', horizontalalignment='left', verticalalignment='bottom')
 
+# 숫자 label 추가
+def add_value_label(x_list, y_list):
+    for i in range(1, len(x_list) + 1):
+        plt.text(i, y_list[i-1], y_list[i-1], fontsize=5, color='purple', horizontalalignment='center',
+                 verticalalignment='bottom')
 
 plt.title('지역별 전기자동차 충전소 현황')
 # x축 라벨 설정
@@ -86,7 +95,37 @@ plt.xlabel('지역')
 # x축 라벨 설정
 plt.ylabel('충전소 개수')
 # 막대 그래프(x, y)
-plt.bar(zcode_arr, df_count)
-add_value_label(zcode_arr,df_count)
+plt.bar(zcode_arr, df_count, color='purple', alpha=0.3)
+add_value_label(zcode_arr, df_count)
 # 그래프 출력
 plt.show()
+
+## 지도 출력
+# x좌표(위도),y좌표(경도) 리스트로 만들기
+x = []
+y = []
+name = []
+state = []
+for i in range(len(station_df['lat'])):
+    if station_df['lat'][i] == 0.0 or station_df['lat'][i] == 0.0:
+        pass
+    else:
+        name.append(station_df['statNm'][i])
+        x.append(station_df['lat'][i])
+        y.append(station_df['lng'][i])
+        state.append(station_df['stat'][i])
+
+#지도 생성
+import folium
+
+m = folium.Map(location=[36.0,126.986],zoom_start=7)
+
+for i in range(len(x)):
+    folium.Circle(
+        location = [x[i], y[i]],
+        tooltip = name[i],
+        radius = 200,
+        color='violet'
+    ).add_to(m)
+
+m.save('station.html')
